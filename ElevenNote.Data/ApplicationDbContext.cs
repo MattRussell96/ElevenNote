097a605ca656ad4936e64499a2dbedfb79288323
Matthew Russell <mrussell5361@gmail.com>
2022-03-22 11:12:03 -0400

    Updated the Database and Added new Migrations

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -10,6 +10,15 @@ namespace ElevenNote.Data
     public class ApplicationDbContext : DbContext
     {
     public DbSet<UserEntity> Users { get; set; }
+    public DbSet<NoteEntity> Notes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<NoteEntity>()
+        .HasOne(n => n.Owner)
+        .WithMany(p => p.Notes)
+        .HasForeignKey(n => n.OwnerId);
+    }
         
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
     : base(options)
